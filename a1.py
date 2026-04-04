d33ce3cf3d88b6ddd93c01da6d6fdde0b64b557f
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-04 17:52:33 +0530

    keywords in python

---
+++ a1.py
@@ -0,0 +1,12 @@
+import keyword
+print(keyword.kwlist)
+print("Hello Iam Yuvraj")
+print("hello", 5 )
+print(" a\n b\n c\n d")
+s = 2
+g = "hi"
+print(s)
+print(g)
+name=input ("hi whats your name?")
+print("hello",name)
+
