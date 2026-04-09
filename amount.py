d3243d1f250f7b1d69d41d73a9293aee164f9c89
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-09 18:44:57 +0530

    operators

---
+++ amount.py
@@ -0,0 +1,9 @@
+Amount = int(input("please enter the amount"))
+
+note1 = Amount//100
+note2 = (Amount%100)//50
+note3 = ((Amount%100)%50)//10
+
+print("notes of 100 rupee", note1)
+print("notes of 50 rupee", note2)
+print("notes of 10 rupee", note3)
