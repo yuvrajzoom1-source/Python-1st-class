64f48f5cbdf217dccc082b6a84906412c4286e2a
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-12 18:10:42 +0530

    temperature

---
+++ summertimes.py
@@ -0,0 +1,5 @@
+light_clothes = float(input("pls enter the temperature"))
+if light_clothes<30:
+    print("this temperature is suitable for wearing light clothes")
+else:
+    print("this temperature is not suitable for wearing light clothes")
