44bee729d0685dc5c533531ce43eabdd5051370a
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-13 17:27:48 +0530

    logicaloperators

---
+++ logicaloperators.py
@@ -0,0 +1,22 @@
+a = 10
+b = -10
+c = 0
+
+if a and b and c:
+    print("All the number have boolean value as true")
+else:
+    print("All the number have boolean  value as false")
+
+a = 10
+b = -10
+c =  0
+
+if a > 0 or b > 0 :
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0 :
+    print("either of the number is greater than 0 ")
+else:
+    print("no number is greater than 0")
