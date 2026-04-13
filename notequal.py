6938d75d807294da826456e892dcfce7b1b4254c
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-13 17:43:20 +0530

    notequal

---
+++ notequal.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 12
+
+print (a!=b)
+print (b!=c)
+
+
+a = "Python"
+b = "Coding"
+
+if a!=b :
+    print(a,"and", b , "are different")
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print("hello")
+
+a = int(input("enter a number?"))
+if a%2 != 0:
+    print(a, " is not an even number")
