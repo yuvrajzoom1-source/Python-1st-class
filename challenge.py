1d2f0843b29c04d9c59528db4788eb762e23a594
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-18 17:30:25 +0530

    challenge

---
+++ challenge.py
@@ -0,0 +1,14 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x/y
+print("Value of (v+w) * x/y", z)
+
+name = "Alex"
+age = 0
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello! Welcome. ")
+else:
+    print("Good Bye !!")
