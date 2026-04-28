e6b1d1d85db4b086c04a2c960b40c291a57f76be
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-04-28 11:46:26 +0530

    power2

---
+++ power2.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent: "))
+result = base ** exponent
+
+print(f"{base} raised to the power of {exponent} is {result}")
