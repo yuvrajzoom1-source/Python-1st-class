4543eb26ae93411dba1fe62e61a56c3715c0a5d8
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-05-02 17:19:52 +0530

    pattern

---
+++ starrightanglepattern.py
@@ -0,0 +1,9 @@
+print ("Half pyramid pattern of stars (*)")
+n = int(input("enter the number of rows"))
+
+for i in range (n):
+
+    for j in range (i+1):
+
+        print("*", end = "")
+    print()
