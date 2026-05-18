0c56ddfbacd69fdacd07ed9ed8e1ca75eb395b75
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-05-18 17:18:22 +0530

    hi

--- hi.py
+++ hi.py
@@ -1,1 +1,9 @@
-print("hi")
+def shutdown(inp):
+    if(inp=="yes"):
+        print("shutting down the system...")
+    elif(inp=="no"):
+        print("abort shut down")
+    else:
+        print("sorry")
+a = input("enter yes/no:")
+shutdown(a)
--- shhut.py
+++
@@ -1,9 +0,0 @@
-def shutdown(inp):
-    if(inp=="yes"):
-        print("shutting down the system...")
-    elif(inp=="no"):
-        print("abort shut down")
-    else:
-        print("sorry")
-a = input("enter yes/no:")
-shutdown(a)
