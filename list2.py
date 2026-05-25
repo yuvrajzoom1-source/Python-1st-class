cefca6d7d5f188309c8caa4e8e29ba4160973293
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-05-25 17:26:09 +0530

    list

---
+++ list2.py
@@ -0,0 +1,12 @@
+emptylist = []
+print(emptylist)
+
+numbers = [1,2,3,4,5]
+print(numbers)
+
+triples = [1,2,3,] * 3
+print(triples)
+
+alist = [100,200,300,400,500]
+alist = alist[::-1]
+print(alist,"\n")
