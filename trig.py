8e79e4f1f94ef8f018ad6bbc7c7c26ff2f17d7ab
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-05-28 09:39:02 +0530

    trig

---
+++ trig.py
@@ -0,0 +1,11 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+
+
+radians = math.radians(angle)
+
+
+print("sin =", math.sin(radians))
+print("cos =", math.cos(radians))
+print("tan =", math.tan(radians))
