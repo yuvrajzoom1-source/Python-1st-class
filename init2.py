6a33806fb238af03cafc752cceca52ffd4e0feae
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-06-09 17:35:42 +0530

    init

---
+++ init2.py
@@ -0,0 +1,14 @@
+class Vehicle:
+
+    def __init__(self,maxspeed, mileage):
+
+        self.maxspeed = maxspeed
+        self.mileage = mileage
+
+
+
+
+modelx = Vehicle(240, 18)
+
+print("Model Max speed:", modelx.maxspeed)
+print("Model Max Mileage:", modelx.mileage)
