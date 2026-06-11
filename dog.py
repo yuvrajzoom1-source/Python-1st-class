50d5774194cfe3e24aa1451617d54cfca385f511
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-06-11 15:18:23 +0530

    dog

---
+++ dog.py
@@ -0,0 +1,23 @@
+class Dog:
+
+
+    animal = "Dog"
+
+
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
+
+
+    def display(self):
+        print("Animal :", Dog.animal)
+        print("Breed  :", self.breed)
+        print("Name   :", self.name)
+        print()
+
+dog1 = Dog("Labrador", "Bruno")
+dog2 = Dog("German Shepherd", "Rocky")
+
+
+dog1.display()
+dog2.display()
