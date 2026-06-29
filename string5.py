feb872a81e84679807bf33351231cc17f7a31d19
yuvrajzoom1-source <yuvrajzoom1@gmail.com>
2026-06-29 16:37:40 +0530

    hi

---
+++ string5.py
@@ -0,0 +1,19 @@
+class WordReverser:
+
+    def reverse_words(self, text: str) -> str:
+
+        words = []
+        for word in text.split(" "):
+            if word:
+                words.append(word)
+
+        reversed_words = []
+        for i in range(len(words) - 1, -1, -1):
+            reversed_words.append(words[i])
+
+        return " ".join(reversed_words)
+
+
+
+reverser = WordReverser()
+print(reverser.reverse_words("hello   world python"))
